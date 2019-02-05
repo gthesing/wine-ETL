cc41970c618a01a547cf8952ce7a9f5a7b6c67a4
Gabe Thesing <gthesing74@gmail.com>
2019-02-04 22:57:47 -0600

    making final changes to project repository

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # wine-reviews-project2
-documents for UT data boot camp project for ETF procedure
+##### documents for UT data boot camp project for ETF procedure
+
+### Order to run files for duplicating this project:
+1. raw_data/geo_data/geo_data.ipynb  
+    * This file made the necessary API calls to Google to get and create a csv file of the geographical data for wineries in our raw data set
+2. db.sql  
+    * Creates the empty database and schema, ready for data load 
+2. ETL_final  
+    * Extracts, transforms, and loads all data to SQL database 
--- db.sql
+++ db.sql
@@ -2,20 +2,39 @@
 DROP DATABASE IF EXISTS winery_db;
 
 -- Create and use winery_db
-CREATE DATABASE winery_db;
+CREATE DATABASE winery_db ;
 USE winery_db;
 
--- Create tables for latitude and longitude data to be loaded into
-CREATE TABLE coordinates (
-  ID int(50) AUTO_INCREMENT,
-  winery VARCHAR (100) CHARACTER SET utf8,
-  latitude DECIMAL(20,5),
-  longitude DECIMAL(20,5),
-  primary KEY(ID)
+-- Create table for location
+CREATE TABLE location (
+    wine_id INT(20),
+    winery VARCHAR(100),
+    province VARCHAR(100),
+    country VARCHAR(100),
+    latitude DECIMAL(5,5),
+    longitude DECIMAL(5,5)
 );
 
-SELECT `coordinates`.`ID`,
-    `coordinates`.`winery`,
-    `coordinates`.`latitude`,
-    `coordinates`.`longitude`
-FROM `winery_db`.`coordinates`;
+-- Create table for review
+CREATE TABLE review (
+    wine_id INT(20),
+    taster_name VARCHAR(100),
+    points INT(20),
+    description VARCHAR(2000),
+    price INT(20)
+);
+
+-- Create table for id
+CREATE TABLE bottle (
+    wine_id INT(20),
+    title VARCHAR(200),
+    variety VARCHAR(100),
+    winery VARCHAR(100)
+);
+
+ALTER DATABASE winery_db CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+ALTER TABLE bottle CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE review CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE location CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+
